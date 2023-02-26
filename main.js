254b3e264e27dde7e6bc6d7518b1b1e8ffc2a9c4
Abhyudya Bhatnagar <abhyudyabhatnagar@live.in>
2023-02-26 13:07:35 +0000

    First commit

---
+++ main.js
@@ -0,0 +1,43 @@
+function onPageLoad(){
+    console.log("JS is running");
+    answer_3();
+    answer_4();
+    
+}
+
+function answer_3(){
+    const form = document.getElementById("hubspotutk");
+    form.addEventListener("submit",function(event){
+        event.preventDefault();
+        document.getElementById("CookieValue").innerHTML = "e18e30e7165779625aa260391f5dc5ee";
+});
+}
+
+function answer_4(){
+    const form = document.getElementById("phonetics");
+
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        var searchTerm = document.getElementById("searchInput").value;
+        console.log(searchTerm);
+        const apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${searchTerm}`;
+
+        fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const phonetic = data[0].phonetics[0].text;
+            if (phonetic == null) {
+                //console.log("No phonetic value found for " + searchTerm);
+                document.getElementById("ans4").innerHTML = "No Phonetic value found for this one. Try another word?"; 
+            } else {
+                //console.log(phonetic);
+                document.getElementById("ans4").innerHTML = phonetic;            
+            }
+        })
+        .catch(error => console.error(error));    
+
+    });
+
+
+}
